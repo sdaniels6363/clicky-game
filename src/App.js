d873ba29672c9e9ab6d838bf04a5fabba7405470
Scott Daniels <sdaniels6363@gmail.com>
2019-11-30 07:45:53 -0500

    page lists cars by alt tag & click function works no images load for now

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import CarCard from "./components/CarCard"
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import cars from "./cars.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    cars
+  };
+
+  toggleClicked = id => {
+    const friends = this.state.friends.filter(friend => friend.id !== id);
+    // Set this.state.friends equal to the new friends array
+    this.setState({ friends });
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Rally Car Memory Game</Title>
+        {this.state.cars.map(cars => (
+          <CarCard
+            id={cars.id}
+            key={cars.id}
+            alt={cars.alt}
+            src={cars.src}
+          />
+        ))};
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
