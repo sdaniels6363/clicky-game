e166b233a9cbaeae9e55ce6b92ce9ad1c2f41913
Scott Daniels <sdaniels6363@gmail.com>
2019-12-02 00:20:56 -0500

    Updates

---
+++ precache-manifest.c3266f4502fb479760d7d05d2e4b423b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a9bc8b8d5a5d295b8ba173fce0b7230",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "623dcaeeb073dc2cc66e",
+    "url": "/clicky-game/static/css/main.10268cac.chunk.css"
+  },
+  {
+    "revision": "d066eecec81a42d0f517",
+    "url": "/clicky-game/static/js/2.b979d32c.chunk.js"
+  },
+  {
+    "revision": "623dcaeeb073dc2cc66e",
+    "url": "/clicky-game/static/js/main.253719f6.chunk.js"
+  },
+  {
+    "revision": "ac1e54977f7772275215",
+    "url": "/clicky-game/static/js/runtime-main.cf7b01c1.js"
+  }
+]);
